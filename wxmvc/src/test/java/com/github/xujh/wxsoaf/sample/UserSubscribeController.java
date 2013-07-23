23275995cde4fd0f5b0c8bf72c6043f4c97c12cb
xujinghui <stevenxu@PC-xu_jh>
2013-07-23 08:31:22 +0800

    1

--- wxmvc/src/test/java/com/github/xujh/wxsoaf/sample/UserSubscribeController.java
+++ wxmvc/src/test/java/com/github/xujh/wxsoaf/sample/UserSubscribeController.java
@@ -3,17 +3,34 @@
  */
 package com.github.xujh.wxsoaf.sample;
 
+import org.springframework.web.servlet.ModelAndView;
+
+import com.github.xujh.wxsoaf.annotation.RequestType;
+import com.github.xujh.wxsoaf.stereotype.RequestMapping;
+import com.github.xujh.wxsoaf.stereotype.WxController;
+
 /**
  * @author stevenxu
- *
+ * 
  */
 @WxController
 public class UserSubscribeController {
 
-	@WxRequestMapping("m") 
-	public ModelAndView showHelloWorld(){
-		
+	@RequestMapping(value = "hello", type = RequestType.text)
+	public String showHello() {
+
+		return "m";
 	}
-	
-	
+
+	@RequestMapping(value = "m", type = RequestType.text)
+	public ModelAndView showMessage() {
+
+		return "redirect:";
+	}
+
+	@RequestMapping(value = "helloworld", type = RequestType.text)
+	public String showHelloworld() {
+
+		return "redirect:hello";
+	}
 }
